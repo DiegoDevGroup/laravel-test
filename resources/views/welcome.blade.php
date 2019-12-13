e0de790dea99424f9558d1d6106b03354feb227f
Eric Van Johnson <eric@diegodev.com>
2019-12-12 18:49:51 -0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,47 +79,37 @@
                 <h1>Basic Laravel Test Repo</h1>
 
                 <p>
-                    This repo is meant to be a test repo for potential junior developers applying for a position.
-                    Nothing about this repo should be too difficult to correct and would honestly take a seasoned
-                    Laravel developer approximately 15 to 30 minutes to complete every task, including the most
-                    Advanced Task, so don't over think the solutions. Most of the time, the simplest solution is the
-                    correct solution. There can also be more that one "right way" of fixing any one of the challenges.
-                    Simple working code always trumps more complex non-working code.
+                    This repository is meant to be a test project for potential junior developers applying for a position. Each task shouldn’t take longer than 30 minutes so don’t overthink your solutions. Most of the time, the simplest solution is the correct solution. There can also be more than one "right way" of fixing any one of the challenges. Simple working code always trumps more complex non-working code.
                 </p>
 
                 <p>
                     If you are using this repo as part of the interview process for the <a href="https://diegodev.com">DiegoDev Group</a>
-                    then know you are not required to complete all task, you are not even required to complete a section,
-                    just simply complete "what you can" and assure everything you complete is working correctly before
-                    submitting your repo for review.
+                     then know you are not required to complete all tasks, you are not even required to complete a section, just simply complete "what you can" and ensure everything you complete is working correctly before submitting your repo for review.
                 </p>
 
 
                 <h2>SetUp</h2>
 
                 <p>
-                    Again, there isn't anything "tricky" about this repo, the application should run on a system
-                    configured to meet requirements of a basic Laravel application. No need to install any other
-                    special PHP Extensions. Most of the task can be completed by simply running the Laravel internal
-                    server. Some of the task require an external datastore(*) such as MySQL or Postgres. * SQLite
-                    should work for many of the task expect for task requiring UUID in the Advanced section.
+                    Again, there isn't anything "tricky" about this repo, the application should run on a system configured to meet the requirements of a basic Laravel application. No need to install any special PHP extensions. Most of the tasks can be completed by simply running the Laravel internal server but you are free to use a tool like Laravel Valet or Docker. Some of the tasks require an external datastore such as MySQL or Postgres. SQLite should work for many of the tasks except for tasks requiring UUIDs in the Advanced section.
                 </p>
 
                 <p><strong>Issues that need to be addressed:</strong></p>
 
-                <h2>Basic Task</h2>
+                <h2>Basic Tasks</h2>
 
                 <ul>
+                    <li> Fork this repo into your Github account.
                     <li> Get application up and running.
                     <li> Fix this list so it justify to the left without losing the format of the rest of page.
-                    <li>Go to route <a href="products">/products</a> and tell me specifically what is wrong and fix it.</li>
+                    <li> Go to route <a href="products">/products</a> and tell me specifically what is wrong, if anything, and fix it.
                     <li> Create a resourceful Controller for a object called Review.
-                    <li> Add a route that points to your Review Controller and make sure it uses a Blade View.
+                    <li> Add a route that points to your Review Controller and have it return a new blade view.
                     <li> Can you explain to me why the route <a href="specials">/specials</a> is failing and fix it.
                     <li> What version of Laravel is this application using?
                 </ul>
 
-                <h2>Simple Task</h2>
+                <h2>Simple Tasks</h2>
 
                 <ul>
                     <li> Populate a database with tables for the project and fill the products table using the seeder.
@@ -133,10 +123,10 @@
                     <li> Create a new branch in your repo and call it `tailwind`. On this branch, upgrade the site to use TailwindCSS.
                 </ul>
 
-                <h2>Advanced Task</h2>
+                <h2>Advanced Tasks</h2>
 
                 <ul>
-                    <li> Sign your commits.
+                    <li> Sign your commits using a GPG key.
                     <li> When adding additional items to the cart of something that had already been added, it is only registering a quantity of 1. Fix this.
                     <li> Add VueJS to repo and make the `Add to cart` a Vue Component that keeps you on the page when adding products to your cart.
                     <li> Upgrade the IDs for products to be UUIDs. Assure everything still works, adding to cart, purchase, and reviews.
@@ -146,7 +136,8 @@
             <div>
                 <h2>Submit your work</h2>
                 <p>
-                    Please copy your `.env` file to `.env.app` and commit it to your repo. Add any answers to the README file.
+                    Please copy your `.env` file to `.env.app` and commit it to your repo. Add any answers to the README file. ZIP up your repo and email it to me. 
+
                 </p>
             </div>
 
